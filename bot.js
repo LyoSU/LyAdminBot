dd774cfe38f747bbbc0848f12b99cd2e1f0b1c58
LyoSU <ua.lyo.su@gmail.com>
2019-01-08 20:06:20 +0200

    banan bot

--- bot.js
+++ bot.js
@@ -2,10 +2,30 @@ const Telegraf = require('telegraf')
 const session = require('telegraf/session')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.use(session())
-bot.on('text', (ctx) => {
-  ctx.reply(`Last message: ${ctx.session.last}`)
-  ctx.session.last = ctx.message.text
-  console.log(ctx.session.last)
+
+bot.telegram.getMe().then((botInfo) => {
+  bot.options.username = botInfo.username
 })
+
+bot.use(session())
+
+bot.command('type', (ctx) =>{
+  console.log(ctx.message)
+  return ctx.reply(`Type: ${ctx.chat.type}`)
+})
+
+bot.command('test', (ctx) =>{
+  return ctx.reply(`name: ${ctx.from.first_name}`)
+})
+
+bot.on('message', (ctx) => {
+  console.log(ctx.message)
+
+  if( ctx.chat.id > 0 ){
+    ctx.reply(`Я работаю только в группах`)
+  }else{
+
+  }
+})
+
 bot.startPolling()
