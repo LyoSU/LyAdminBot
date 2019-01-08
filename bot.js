5f352f42b8e14c09da1bde5a622789949e5beeec
LyoSU <ua.lyo.su@gmail.com>
2019-01-08 20:16:38 +0200

    ++

--- bot.js
+++ bot.js
@@ -9,21 +9,30 @@ bot.telegram.getMe().then((botInfo) => {
 
 bot.use(session())
 
-bot.command('type', (ctx) =>{
-  console.log(ctx.message)
-  return ctx.reply(`Type: ${ctx.chat.type}`)
+function userLogin(from){
+  login = from.first_name
+  if(from.last_name) login += " " + from.last_name
+  return login
+}
+
+bot.command('help', (ctx) => {
+  return ctx.replyWithHTML(`<b>${userLogin(ctx.from)}</b> пидор дня`)
 })
 
-bot.command('test', (ctx) =>{
-  return ctx.reply(`name: ${ctx.from.first_name}`)
+bot.command('type', (ctx) => {
+  return ctx.replyWithHTML(`<b>Chat type:</b> <pre>${ctx.chat.type}</pre>`)
+})
+
+bot.command('test', (ctx) => {
+  return ctx.replyWithHTML(`name: ${userLogin(ctx.from)}`)
 })
 
 bot.on('message', (ctx) => {
   console.log(ctx.message)
 
-  if( ctx.chat.id > 0 ){
+  if( ctx.chat.id > 0 ) {
     ctx.reply(`Я работаю только в группах`)
-  }else{
+  } else {
 
   }
 })
