5506b89e35de17abeb78552459f1ad74c0917042
LyoSU <ua.lyo.su@gmail.com>
2019-01-13 04:02:31 +0200

    reset fix

--- common/group-settings-reset.js
+++ common/group-settings-reset.js
@@ -3,7 +3,7 @@ const Group = require('../models/group')
 module.exports = async (groupId, callback) => {
   await Group.update(
     { group_id: groupId },
-    { 'settings.gifs': null, 'settings.texts': null }, (err, doc) => {
+    { $unset: {'settings': 1} }, (err, doc) => {
       if (err) return console.log(err)
       if (callback && typeof callback === 'function') callback()
     }
