f2192950b85e20a4f3d925810e4af0532094e084
LyoSU <ua.lyo.su@gmail.com>
2019-01-14 17:18:57 +0200

    fix

--- models/group.js
+++ models/group.js
@@ -50,17 +50,6 @@ const groupSchema = mongoose.Schema({
       default: texts
     }
   },
-  members: [
-    {
-      telegram_id: {
-        type: Number,
-        index: true,
-        unique: true
-      },
-      first_act: Number,
-      last_act: Number
-    }
-  ],
   first_act: Number,
   last_act: Number
 })
