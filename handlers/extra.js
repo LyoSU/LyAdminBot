4b4c88d2688e6a1d1d81591db452c6fdb6f45a73
LyoSU <ua.lyo.su@gmail.com>
2019-04-22 19:23:39 +0300

    1.3.0

--- handlers/extra.js
+++ handlers/extra.js
@@ -3,26 +3,29 @@ const Group = require('../models/group')
 
 
 module.exports = async (ctx, next) => {
-  const extraText = ctx.message.text.slice(1)
+  ctx.message.entities.forEach(async (entity) => {
+    if (entity.type === 'hashtag') {
+      const hashtag = ctx.message.text.substring(entity.offset, entity.offset + entity.length)
 
-  const groupExtra = await Group.findOne({
-    group_id: ctx.chat.id,
-    'settings.extras.name': { $regex: `^${extraText}$`, $options: 'i' },
-  }, { 'settings.extras.$': 1 }).catch(console.log)
+      const groupExtra = await Group.findOne({
+        group_id: ctx.chat.id,
+        'settings.extras.name': { $regex: `^${hashtag.slice(1)}$`, $options: 'i' },
+      }, { 'settings.extras.$': 1 }).catch(console.log)
 
-  if (groupExtra) {
-    const extra = groupExtra.settings.extras[0]
+      if (groupExtra) {
+        const extra = groupExtra.settings.extras[0]
 
-    if (ctx.message.reply_to_message) {
-      extra.message.reply_to_message_id = ctx.message.reply_to_message.message_id
+        if (ctx.message.reply_to_message) extra.message.reply_to_message_id = ctx.message.reply_to_message.message_id
+        else extra.message.reply_to_message_id = ctx.message.message_id
+
+        const method = replicators.copyMethods[extra.type]
+        const opts = Object.assign({ chat_id: ctx.chat.id }, extra.message)
+
+        ctx.telegram.callApi(method, opts)
+      }
+      else {
+        next()
+      }
     }
-
-    const method = replicators.copyMethods[extra.type]
-    const opts = Object.assign({ chat_id: ctx.chat.id }, extra.message)
-
-    ctx.telegram.callApi(method, opts)
-  }
-  else {
-    next()
-  }
+  })
 }
