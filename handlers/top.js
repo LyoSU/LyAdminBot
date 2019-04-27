869fcb9ae0d158c3181d5d3240881d496b316937
LyoSU <ua.lyo.su@gmail.com>
2019-04-27 20:23:00 +0300

    1.3.8

---
+++ handlers/top.js
@@ -0,0 +1,38 @@
+const User = require('../models/user')
+const { userName } = require('../utils')
+
+
+module.exports = async (ctx) => {
+
+  let topMembers = []
+
+  ctx.groupInfo.members.forEach((member) => {
+    const active = ((member.stats.textTotal * 100) / ctx.groupInfo.stats.textTotal).toFixed(2)
+
+    topMembers.push({
+      telegram_id: member.telegram_id,
+      active,
+    })
+  })
+
+  if (topMembers.length > 0) {
+    topMembers.sort((a, b) => b.capital - a.capital)
+
+    let top = ''
+
+    topMembers = topMembers.slice(0, 10)
+
+    for (let index = 0; index < topMembers.length; index++) {
+      const user = await User.findOne({ telegram_id: topMembers[index].telegram_id })
+
+      top += `\n${userName(user)} — ${topMembers[index].active}%`
+    }
+
+    ctx.replyWithHTML(ctx.i18n.t('cmd.top.info', {
+      chatName: ctx.chat.title,
+      top,
+    }), {
+      reply_to_message_id: ctx.message.message_id,
+    })
+  }
+}
