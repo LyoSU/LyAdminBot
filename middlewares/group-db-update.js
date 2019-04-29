1c2b0e407c3de5860d81d18863fd954a37a3ed9e
LyoSU <ua.lyo.su@gmail.com>
2019-04-29 21:36:31 +0300

    1.3.10

--- middlewares/group-db-update.js
+++ middlewares/group-db-update.js
@@ -2,7 +2,5 @@ const Group = require('../models/group')
 
 
 module.exports = async (ctx) => {
-  if (['supergroup', 'group'].includes(ctx.chat.type)) {
-    await Group.dbUpdate(ctx)
-  }
+  await Group.dbUpdate(ctx)
 }
