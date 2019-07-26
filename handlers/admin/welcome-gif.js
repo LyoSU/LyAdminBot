e1d36a88e4046688b6c61092df7986bc9079d7f4
LyoSU <ua.lyo.su@gmail.com>
2019-07-26 19:19:09 +0300

    1.6.7

--- handlers/admin/welcome-gif.js
+++ handlers/admin/welcome-gif.js
@@ -2,24 +2,17 @@ module.exports = async (ctx) => {
   if (ctx.message.reply_to_message.animation) {
     const gifId = ctx.message.reply_to_message.animation.file_id
 
-    const groupGifs = await ctx.db.Group.findOne({
-      group_id: ctx.chat.id,
-      'settings.welcome.gifs': { $in: [gifId] },
-    }, { 'settings.welcome.$': 1 }).catch(console.log)
+    const groupGifs = ctx.group.info.settings.welcome.gifs.findIndex((el) => {
+      if (el === gifId) return true
+    })
 
-    if (groupGifs) {
-      await ctx.db.Group.update(
-        { group_id: ctx.chat.id },
-        { $pull: { 'settings.welcome.gifs': gifId } }
-      ).catch(console.log)
-      await ctx.replyWithHTML(ctx.i18n.t('cmd.gif.pull')).catch(console.log)
+    if (groupGifs < 0) {
+      ctx.group.info.settings.welcome.gifs.push(gifId)
+      ctx.replyWithHTML(ctx.i18n.t('cmd.gif.push')).catch(console.log)
       return
     }
 
-    await ctx.db.Group.update(
-      { group_id: ctx.chat.id },
-      { $push: { 'settings.welcome.gifs': gifId } }
-    ).catch(console.log)
-    await ctx.replyWithHTML(ctx.i18n.t('cmd.gif.push')).catch(console.log)
+    delete ctx.group.info.settings.welcome.gifs[gifId]
+    ctx.replyWithHTML(ctx.i18n.t('cmd.gif.pull')).catch(console.log)
   }
 }
