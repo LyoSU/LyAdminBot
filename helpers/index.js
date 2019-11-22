3996eba352ab07267312f663ad3ba52bc31ab5c4
LyoSU <mail@lyo.su>
2019-11-23 00:38:09 +0200

    1.12.0

--- helpers/index.js
+++ helpers/index.js
@@ -1,10 +1,11 @@
 const updateUser = require('./user-update')
 const updateGroup = require('./group-update')
 const updateGroupMember = require('./group-member-update')
-
+const emojipedia = require('./emoji')
 
 module.exports = {
   updateUser,
   updateGroup,
   updateGroupMember,
+  emojipedia
 }
