652633a7ac5dce6ca81c51e8e1bdbdd2b4f552d4
LyoSU <mail@lyo.su>
2019-11-23 14:18:47 +0200

    1.12.1

--- helpers/emoji/index.js
+++ helpers/emoji/index.js
@@ -1,18 +1,11 @@
+const path = require('path')
+
 // author: https://github.com/seiya-git
 const EmojiSearchDb = require('./module.emoji-searcher')
 const extModule = require('./module.extended')
 
-const emojiDb = extModule.loadDb(__dirname + '/emoji-data/', 'emojilist_', '.json')
+const emojiDb = extModule.loadDb(path.join(__dirname, '/emoji-data/'), 'emojilist_', '.json')
 const emojiSearch = new EmojiSearchDb(emojiDb)
-
-for (const i in emojiDb) {
-  if (i.match(/fe0f-200d/)) {
-    let r = i.replace(/fe0f-200d/g, '200d')
-    if (emojiDb[r] === undefined) {
-      emojiDb[r] = { redirect: i }
-    }
-  }
-}
 
 const getEmoji = (chart) => {
   return emojiSearch.getEmojis(chart)
--- helpers/emoji/module.extended.js
+++ helpers/emoji/module.extended.js
@@ -1,30 +1,4 @@
 module.exports = {
-  log: (type, ...args) => {
-    const util = require('util')
-    const s = util.format(...args)
-    if (type === 'ERROR') {
-      console.error('[%o]: %s', new Date(), s) // eslint-disable-line no-console
-    } else {
-      console.log('[%o]: %s', new Date(), s) // eslint-disable-line no-console
-    }
-  },
-  emojiDb: {
-    host: 'http://emojipedia.org/',
-    prefix: 'emoji-',
-    ver: {
-      base: [1, 2, 3, 4, 5, 11, 12, 12.1],
-      comp: [1, 2, 5, 11]
-    }
-  },
-  lang: {
-    hello: "Hello! I'm unofficial Emojipedia.org bot.",
-    notfound_text: 'Emoji not found. Please send me emoji or sticker!',
-    notfound_db: 'Emoji "{emoji}" not found in database!',
-    too_many: 'Too many emoji!'
-  },
-  uniqueArr: (arr) => {
-    return [...new Set(arr)]
-  },
   loadDb: (dbDir, filePrefx, fileSuffx) => {
     const fs = require('fs')
     let dbFiles = fs.readdirSync(dbDir)
