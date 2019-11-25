b458af6e6f652fd31e603e5b1fb090a54b66371e
LyoSU <ua.lyo.su@gmail.com>
2019-11-25 19:52:51 +0200

    1.14.0

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,17 @@
 const userName = require('./user-name')
 const getRandomInt = require('./get-random-int')
-
+const emojipedia = require('./emojipedia')
+const generateQuote = require('./quote-generate')
+const loadCanvasImage = require('./canvas-image-load')
+const loadImageFromUrl = require('./image-load-url')
+const loadImageFromPath = require('./image-load-path')
 
 module.exports = {
   userName,
   getRandomInt,
+  emojipedia,
+  generateQuote,
+  loadCanvasImage,
+  loadImageFromUrl,
+  loadImageFromPath
 }
