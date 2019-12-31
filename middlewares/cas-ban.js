1796fbe26207760256391d57f2dc35d51f2de779
LyoSU <mail@lyo.su>
2019-12-31 19:25:31 +0200

    1.18.4 remove log

--- middlewares/cas-ban.js
+++ middlewares/cas-ban.js
@@ -12,8 +12,8 @@ module.exports = async (ctx, next) => {
     const userId = ctx.from.id
 
     extend.get(`https://api.cas.chat/check?user_id=${userId}`).then(({ body }) => {
-      console.log(body)
       if (body.ok === true) {
+        console.log(body)
         ctx.telegram.kickChatMember(ctx.chat.id, userId)
         ctx.replyWithHTML(ctx.i18n.t('cas.banned', {
           name: userName(ctx.from, true),
