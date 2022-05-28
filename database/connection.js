d1584c9ea07c94743bedbf125d87971c1eaf8e3e
Yuri Ly <mail@lyo.su>
2022-05-28 08:17:11 +0300

    1.21.7 fix mongo connection

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
 
 const connection = mongoose.createConnection(process.env.MONGODB_URI, {
+  poolSize: 10,
+  maxTimeMS: 3,
   useUnifiedTopology: true,
   useNewUrlParser: true
+})
+
+connection.on('error', error => {
+  console.error(error)
 })
 
 module.exports = connection
--- database/models/user.js
+++ database/models/user.js
@@ -1,20 +1,18 @@
 const mongoose = require('mongoose')
 
-
 const userSchema = mongoose.Schema({
   telegram_id: {
     type: Number,
     index: true,
     unique: true,
-    required: true,
+    required: true
   },
   first_name: String,
   last_name: String,
   username: String,
-  locale: String,
+  locale: String
 }, {
-  timestamps: true,
+  timestamps: true
 })
-
 
 module.exports = userSchema
