e56a5efd00c28f9ea7b72c1b0e33f097542042ce
Yuri Ly <mail@lyo.su>
2022-06-03 17:21:08 +0300

    enable pm2 watch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: './index.js',
     max_memory_restart: '1000M',
     // instances: 1,
-    // watch: true,
+    watch: true,
     ignore_watch: ['node_modules', 'assets'],
     env: {
       NODE_ENV: 'development'
