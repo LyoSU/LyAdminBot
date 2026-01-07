372e8968db99a90c6c2ba213023d54f0277c8e5c
Yuri Ly <yuri@lyubchak.com>
2026-01-07 15:35:06 +0200

    Fix user name retrieval by adding null check for user object and ensuring proper string conversion for HTML escaping

--- utils/user-name.js
+++ utils/user-name.js
@@ -1,8 +1,10 @@
 module.exports = (user, url = false) => {
-  let name = user.first_name
+  if (!user) return 'Unknown'
+
+  let name = user.first_name || user.title || user.username || `ID:${user.id}` || 'Unknown'
 
   if (user.last_name) name += ` ${user.last_name}`
-  name = name.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+  name = String(name).replace(/</g, '&lt;').replace(/>/g, '&gt;')
 
   if (url) return `<a href="tg://user?id=${user.id}">${name}</a>`
   return name
